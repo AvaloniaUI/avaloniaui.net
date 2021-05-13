bd67cbd6680066c9ee2f6184dd3caa57c0a8b434
Mike James <michael.james6@gmail.com>
2021-05-13 12:40:46 +0100

    Started migration

---
+++ src/AvaloniaUI/Pages/Support.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AvaloniaUI.Pages
+{
+    public class SupportModel : PageModel
+    {
+        private readonly ILogger<SupportModel> _logger;
+
+        public SupportModel(ILogger<SupportModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
